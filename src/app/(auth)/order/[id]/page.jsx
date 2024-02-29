85706fad999e093511b5809eb30e3b52e84aba2a
crokids <148979461+crokids@users.noreply.github.com>
2024-02-29 16:40:21 -0300

    Update page.jsx

--- src/app/(auth)/order/[id]/page.jsx
+++ src/app/(auth)/order/[id]/page.jsx
@@ -2,14 +2,31 @@ import React from 'react'
 import BackGround from '@/components/BackGround'
 import MainFormPedido from '@/components/form/MainFormPedido'
 import { GetProducts } from '@/utils/GetClienteData'
+import { getXataClient } from '@/lib/xata'
+
+const xata= getXataClient()
 
 export default async function OrderPage({params}) {
 
       const products = await GetProducts()
-  
+
+      const sendOrder = async(data, cod, nf, pgt) => {
+        'use server'
+
+        const record = await xata.db.Pedidos.create({
+          Data: new Date(),
+          link: params.id,
+          cod_vendedor: cod,
+          tipo_pgt: pgt,
+          nf: nf,
+          produtos: data,
+        })
+
+          return 'ok'
+      }  
   return (
     <div className="flex w-full h-full">
-      <BackGround MainFormPedido={<MainFormPedido id={params.id} products={products}/>} />
+      <BackGround MainFormPedido={<MainFormPedido id={params.id} products={products} sendOrder={sendOrder} />} />
     </div>
   )
 }
