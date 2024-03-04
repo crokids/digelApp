c0ce12be9ad80ea75325fefc7ded9c5f7e19e465
crokids <148979461+crokids@users.noreply.github.com>
2024-03-04 16:31:36 -0300

    Update CheckOut.jsx

--- src/components/form/CheckOut.jsx
+++ src/components/form/CheckOut.jsx
@@ -4,13 +4,13 @@ import { List, Button, message } from 'antd'
 import { useUser } from '@clerk/clerk-react'
 import { useRouter } from 'next/navigation'
 
-export default function CheckOutForm({cart, tipo_pgt, nf, fazpedido}) {
+export default function CheckOutForm({cart, tipo_pgt, nf, fazpedido, obs}) {
         const router = useRouter()
         const [messageApi, contextHolder] = message.useMessage();
         const [loading, setLoading] = useState(false)
         const { user } = useUser();
         const vendedorId = user.publicMetadata.cod
-        const vendedorNome = user.fullName
+        const vendedorNome = user.lastName
 
         const success = () => {
             messageApi.open({
@@ -20,17 +20,19 @@ export default function CheckOutForm({cart, tipo_pgt, nf, fazpedido}) {
           };
 
       const data2 = cart.map(item => `${item.produto} : ${item.quantidade}`);
+
     
       const data1 = [
-        `Vendedor: ${vendedorNome}`,
-        `Código do vendedor: ${vendedorId}`,
+        `Rota: ${vendedorNome}`,
+        `Código da rota: ${vendedorId}`,
         `Nota fiscal: ${nf ? 'sim' : 'não'}`,
         `Tipo de pagamento: ${tipo_pgt ? 'à vista' : 'a prazo'}`,
+        `Observação: ${obs ? obs : 'sem observações'}`
     ];
 
         const onButtonClick = async () => {
             setLoading(true)
-            const res = await fazpedido(cart, vendedorId, nf, tipo_pgt )
+            const res = await fazpedido(cart, vendedorId, nf, tipo_pgt, obs )
             if (res === 'ok') {
                 setLoading(false)
                 success()
