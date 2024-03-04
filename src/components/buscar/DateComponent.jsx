878acae88a56de707651886176259b536a731bc3
crokids <148979461+crokids@users.noreply.github.com>
2024-03-04 15:27:00 -0300

    Update DateComponent.jsx

--- src/components/buscar/DateComponent.jsx
+++ src/components/buscar/DateComponent.jsx
@@ -1,15 +1,17 @@
 'use client'
 
 import React, { useState } from 'react';
-import { DatePicker } from 'antd';
+import { DatePicker, Button } from 'antd';
+import { FileExcelOutlined } from '@ant-design/icons'
 import locale from 'antd/es/date-picker/locale/pt_BR';
 import 'dayjs/locale/pt-br';
 import dayjs from 'dayjs';
 
 const { RangePicker } = DatePicker;
 
-export default function DateComponent({searchPedidos}) {
+export default function DateComponent({searchPedidos, getData}) {
     const [dateRange, setDateRange] = useState([dayjs().startOf('day'), dayjs().endOf('day')]);
+    const [data, setData] = useState([])
 
     const handleDateChange = async (dates) => {
         if (dates) {
@@ -18,13 +20,73 @@ export default function DateComponent({searchPedidos}) {
            const res = await searchPedidos(strigFy)
 
             const parsedRes = JSON.parse(res)
-            console.log(parsedRes)
+            getData(parsedRes)
+            setData(parsedRes)
         }
     };
 
+    function downloadCSV(data) {
+        if (!data.length) {
+            return;
+        }
+    
+        let csvContent = 'Data;cod_vendedor;tipo_pgt;nf;'; 
+        let allProducts = [];
+    
+        data.forEach(order => {
+            order.produtos.forEach(product => {
+                if (!allProducts.includes(product.produto)) {
+                    allProducts.push(product.produto);
+                    csvContent += `Produto;Quantidade;`; 
+                }
+            });
+        });
+    
+        csvContent += '\n';
+    
+        data.forEach(order => {
+            const formattedDate = dayjs(order.Data).locale('pt-BR').format('DD/MM/YYYY')
+            csvContent += `${formattedDate};${order.cod_vendedor};${order.tipo_pgt};${order.nf};`;
+    
+            allProducts.forEach(productName => {
+                const product = order.produtos.find(p => p.produto === productName);
+                if (product) {
+                    csvContent += `${product.produto};${product.quantidade};`;
+                } else {
+                    csvContent += ';;';
+                }
+            });
+    
+            csvContent += '\n';
+        });
+    
+        const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    
+        const url = URL.createObjectURL(blob);
+    
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'Pedidos.csv');
+        document.body.appendChild(link);
+        link.click();
+    
+        document.body.removeChild(link);
+    }
+    
+    
+
     return (
-        <div>
+        <div className='flex flex-row w-full justify-center gap-10'>
+            <div>
             <RangePicker format={'DD/MM/YYYY'} locale={locale} defaultValue={dateRange} onChange={handleDateChange} />
+            </div>
+            {data.length > 0 && (
+            <div>
+            <Button 
+            onClick={() => downloadCSV(data)} 
+            icon={<FileExcelOutlined style={{fontSize: '18px'}} />}>download</Button>
+            </div>
+            )}     
         </div>
     );
 }
