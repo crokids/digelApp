7c314ae79afe8e785b6790b0233da793d4ec29b6
crokids <148979461+crokids@users.noreply.github.com>
2024-03-04 15:28:08 -0300

    Update BuscarTable.jsx

--- src/components/buscar/BuscarTable.jsx
+++ src/components/buscar/BuscarTable.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+'use client'
 
-export default function BuscarTable() {
+import React from 'react';
+import { Space, Table } from 'antd';
+
+const columns = [
+  {
+    title: 'Data',
+    dataIndex: 'data',
+    key: 'data',
+  },
+  {
+    title: 'Cod_cli',
+    dataIndex: 'cliente',
+    key: 'cliente',
+  },
+  {
+    title: 'Cod_rota',
+    dataIndex: 'cod_rota',
+    key: 'cod_rota',
+  },
+  {
+    title: 'NF',
+    dataIndex: 'nf',
+    key: 'nf',
+  },
+  {
+    title: 'Tipo_pgt',
+    dataIndex: 'tipo_pgt',
+    key: 'tipo_pgt',
+  },
+  {
+    title: 'Produtos',
+    dataIndex: 'produtos',
+    key: 'produtos',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Editar</a>
+        <a>Deletar</a>
+      </Space>
+    ),
+  },
+];
+
+export default function BuscarTable({tableData}) {
+
+  if(!tableData) {
+
+    return 
+    
+  }
+
+
+  const transformedData = tableData.map((item, index) => {
+    const formattedDate = new Date(item.Data).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+
+    const nf = item.nf ? 'sim' : 'não';
+    const tipo_pgt = item.tipo_pgt ? 'à vista' : 'a prazo';
+  
+    const produtos = item.produtos.map((produto) => (
+      <div key={produto.id}>
+        {produto.produto}: {produto.quantidade}
+      </div>
+    ));
+  
+    return {
+      key: item.id,
+      data: formattedDate,
+      cliente: item.link.COD,
+      cod_rota: item.cod_vendedor,
+      nf: nf,
+      tipo_pgt: tipo_pgt,
+      produtos: produtos,
+    };
+  });
+  
+
   return (
-    <div>
-        
+    <div className='flex w-full h-full justify-center'>
+        <Table style={{width: '80%'}} columns={columns} dataSource={transformedData} />
     </div>
   )
 }
