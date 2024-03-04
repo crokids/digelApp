f0743269c72c55d7db8f7b9fc8d27eea9e779fc3
crokids <148979461+crokids@users.noreply.github.com>
2024-03-04 15:27:31 -0300

    Update BuscarMain.jsx

--- src/components/buscar/BuscarMain.jsx
+++ src/components/buscar/BuscarMain.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import DateComponent from './DateComponent'
 import BuscarTable from './BuscarTable'
 
 export default function BuscarMain({searchPedidos}) {
+  const [tableData, setTableData] = useState([])
+
+
+  const getData = (data) => {
+    setTableData(data)
+  }
+
   return (
-    <div className='flex flex-col w-full h-full'>
-        <di className='flex items-center justify-center h-[20%]'>
-        <DateComponent searchPedidos={searchPedidos} />
+    <div className='flex flex-col w-full h-full gap-10'>
+        <di className='flex items-center justify-center'>
+        <DateComponent searchPedidos={searchPedidos} getData={getData} />
         </di>
-        <div className='flex items-center justify-center h-[80%]'>
-        <BuscarTable />
+        <div className='flex items-center justify-center overflow-auto'>
+         <BuscarTable tableData={tableData} />
         </div>
     </div>
   )
