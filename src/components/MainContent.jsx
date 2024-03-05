7b59185d35d6c40b3ea91044bae54448f69827d2
crokids <148979461+crokids@users.noreply.github.com>
2024-03-05 16:14:48 -0300

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,50 @@
+'use client'
 import React from 'react'
-import { auth, currentUser  } from '@clerk/nextjs'
+import { Card, Col, Statistic, Typography } from 'antd'
 
-export default async function MainContent() {
+const { Title } = Typography
 
-  const user = await currentUser()
+export default function MainContent({data}) {
+      const currentDate = new Date()
+      const currentMonth = currentDate.getMonth()
+      const monthNames = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+      const currentMonthName = monthNames[currentMonth]
 
   return (
-    <div>{user.username}</div>
+    <>
+    <Title level={3}>
+    Quantidade de pedidos no mês de {currentMonthName}
+    </Title>
+    <div className='flex flex-row w-full gap-5'>
+      <div className='flex w-[50%] justify-center'>
+              <Col span={12}>
+              <Card bordered={true}>
+                <Statistic
+                  title="Rota Interior"
+                  value={data.aggs.vendedor1}
+                  precision={2}
+                  valueStyle={{
+                    color: 'Blue',
+                  }}
+                />
+              </Card>
+            </Col>
+      </div>
+      <div className='flex justify-center w-[50%]'>
+            <Col span={12}>
+            <Card bordered={true}>
+              <Statistic
+                title="Rota Cidade"
+                value={data.aggs.vendedor2}
+                precision={2}
+                valueStyle={{
+                  color: 'Purple',
+                }}
+              />
+            </Card>
+          </Col>
+      </div>
+    </div>
+    </>
   )
 }
