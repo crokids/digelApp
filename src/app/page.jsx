01f39516db372e494e4d33b166d2c5d8cc50f761
crokids <148979461+crokids@users.noreply.github.com>
2024-03-05 16:14:24 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,56 @@
 import React from "react"
 import BackGround from "@/components/BackGround";
 import MainContent from "@/components/MainContent";
+import { getXataClient } from "@/lib/xata";
+
+const xata = getXataClient()
 
 export default async function Home() {
 
+  const currentDate = new Date();
+
+  const month = currentDate.getMonth() + 1
+
+  const startDate = `2024-${month}-01`
+  const endDate = `2024-${month}-31`
+
+  const data = await xata.db.Pedidos.aggregate({
+    vendedor1: {
+      count: {
+        filter: {
+          $all: [
+            {
+              Data: { $ge: new Date(startDate) },
+            },
+            {
+              Data: { $lt: new Date(endDate) },
+            },
+          ],
+          cod_vendedor: 9,
+        },
+      },
+    },
+    vendedor2: {
+      count: {
+        filter: {
+          $all: [
+            {
+              Data: { $ge: new Date(startDate) },
+            },
+            {
+              Data: { $lt: new Date(endDate) },
+            },
+          ],
+          cod_vendedor: 8,
+        },
+      },
+    },
+  });
 
   return (
 
     <div className="flex w-full h-full">
-      <BackGround mainContent={<MainContent/>}/>
+      <BackGround mainContent={<MainContent data={data} />}/>
     </div>
   
   );
