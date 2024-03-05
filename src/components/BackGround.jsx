4698e789cbc5c02236f5b5e4e847bb9a737836da
crokids <148979461+crokids@users.noreply.github.com>
2024-03-05 16:17:06 -0300

    Update BackGround.jsx

--- src/components/BackGround.jsx
+++ src/components/BackGround.jsx
@@ -1,12 +1,13 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { UserOutlined, LeftCircleFilled, SearchOutlined, 
   ShopOutlined,
   SendOutlined
 } from '@ant-design/icons';
 import { Layout, Menu, theme } from 'antd';
 import { UserButton } from '@clerk/nextjs';
+import { useUser } from '@clerk/clerk-react'
 
 const { Sider, Content, Footer } = Layout;
 
@@ -14,9 +15,62 @@ const { Sider, Content, Footer } = Layout;
 const BackGround = ({ content, mainContent, MainFormPedido, Order, buscar }) => {
   const [margin, setMargin] = useState(80);
   const [collapsed, setCollapsed] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false)
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  const { user } = useUser()
+
+  useEffect(() => {
+    if (user && user.username === 'admin') {
+      setIsAdmin(true);
+    }
+  }, [user]);
+
+
+
+  const menuItems = [
+    {
+      key: '1',
+      icon: <UserButton afterSignOutUrl='/'/>,
+      style: {marginBottom: '30px', display: 'flex', justifyContent: 'center', alignItems: 'center'}
+    },
+    {
+      key: '2',
+      icon: <LeftCircleFilled />,
+      label: <a href='/'>voltar</a>,
+      style: { marginBottom: '10px' }
+    },
+    {
+      key: '3',
+      icon: <SendOutlined />,
+      label: <a href='/order'>Pedido</a>,
+      style: { marginBottom: '10px' }
+    },
+    {
+      key: '4',
+      icon: <UserOutlined />,
+      label: <a href='/tableclient'>Cliente</a>,
+      style: { marginBottom: '10px' }
+    },
+    {
+      key: '5',
+      icon: <SearchOutlined />,
+      label: <a href='/pedidos'>buscar</a>,
+      style: { marginBottom: '10px' }
+    },
+    {
+      key: '6',
+      icon: <ShopOutlined />,
+      label: <a href='#'>Estoque</a>,
+      style: { marginBottom: '10px' }
+    }
+  ];
+
+
+
+  const finalMenuItems = isAdmin ? menuItems : menuItems.slice(0, -2)
 
 
   return (
@@ -44,45 +98,7 @@ const BackGround = ({ content, mainContent, MainFormPedido, Order, buscar }) => 
           theme="dark"
           selectable={false}
           mode="inline"
-          items={[
-            {
-              key: '5',
-              icon: <UserButton afterSignOutUrl='/'/>,
-              style: {marginBottom: '30px', display: 'flex', justifyContent: 'center', alignItems: 'center'}
-            },
-            {
-              key: '1',
-              icon: <LeftCircleFilled />,
-              label: <a href='/'>voltar</a>,
-              style: {marginBottom: '10px'}
-            },
-            {
-              key: '6',
-              icon: <SendOutlined />,
-              label: <a href='/order'>Pedido</a>,
-              style: {marginBottom: '10px'}
-            },
-            {
-              key: '2',
-              icon: <UserOutlined />,
-              label: <a href='/tableclient'>Cliente</a>,
-              style: {marginBottom: '10px'}
-            },
-            {
-              key: '3',
-              icon: <SearchOutlined />,
-              label: <a href='/pedidos'>buscar</a>,
-              style: {marginBottom: '10px'}
-            },
-            {
-              key: '4',
-              icon: <ShopOutlined />,
-              label: <a href='#'>Estoque</a>,
-              style: {marginBottom: '10px'}
-            },
-           
-
-          ]}
+          items={finalMenuItems}
           style={{ marginTop: '30px' }}
         />
       </Sider>
