d83730a563dab51720aea9a53502e6583ad7de22
crokids <148979461+crokids@users.noreply.github.com>
2024-03-05 16:13:08 -0300

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 const RootLayout = ({ children }) => (
-  <html lang="en" className='flex h-full w-full'>
+  <html lang="pt-BR" className='flex h-full w-full'>
     <ClerkProvider>
     <body className='flex h-full w-full'>
       <AntdRegistry>{children}</AntdRegistry>
