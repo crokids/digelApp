6a1f13df40ca813d3b1350117645b4132f3fdde3
crokids <148979461+crokids@users.noreply.github.com>
2024-03-14 15:58:28 -0300

    Update DateComponent.jsx

--- src/components/buscar/DateComponent.jsx
+++ src/components/buscar/DateComponent.jsx
@@ -1,7 +1,7 @@
 'use client'
 
-import React, { useState } from 'react';
-import { DatePicker, Button } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { DatePicker, Button, Select } from 'antd';
 import { FileExcelOutlined } from '@ant-design/icons'
 import locale from 'antd/es/date-picker/locale/pt_BR';
 import 'dayjs/locale/pt-br';
@@ -12,16 +12,26 @@ const { RangePicker } = DatePicker;
 export default function DateComponent({searchPedidos, getData}) {
     const [dateRange, setDateRange] = useState([dayjs().startOf('day'), dayjs().endOf('day')]);
     const [data, setData] = useState([])
+    
+    const callBack = async (cod) => {
+
+        const strigFy = JSON.stringify(dateRange)
+        const res = await searchPedidos(strigFy, cod)
+
+         const parsedRes = JSON.parse(res)
+         getData(parsedRes)
+         setData(parsedRes)
+
+    }
+
+    
+    const handleSelectChange = (value) => {
+            callBack(value)
+    }
 
     const handleDateChange = async (dates) => {
         if (dates) {
             setDateRange(dates);
-            const strigFy = JSON.stringify(dates)
-           const res = await searchPedidos(strigFy)
-
-            const parsedRes = JSON.parse(res)
-            getData(parsedRes)
-            setData(parsedRes)
         }
     };
 
@@ -30,15 +40,15 @@ export default function DateComponent({searchPedidos, getData}) {
             return;
         }
     
-        // Define the list of all products
         const allProducts = [
             '01-30GR QUEIJO', '02-30GR CEBOLA', '04-30GR G.CAIPIRA',
             '05-30GR PRESUNTO', '07-30GR CHURRASCO', '11-60GR S.QUEIJAO',
             '12-60GR S.CEBOLA', '13-60GR S.GALINHA', '14-60GR S.PRESUNTO',
-            '22-50GR CEBOLA', '25-50GR PALITAO'
+            '21–40GR QUEIJO', '22-40GR CEBOLA', '23-40GR GALINHA','24-40GR PRESUNTO',
+            '31–50GR QUEIJO', '32-50GR CEBOLA', '33-50GR GALINHA', '34-50GR PRESUNTO',
+            '35-50GR PALITAO'
         ];
     
-        // Create CSV header
         let csvContent = 'Data;Cod_cli;cod_vendedor;tipo_pgt;nf;obs;';
         allProducts.forEach((product, index) => {
             csvContent += `Produto${index + 1};Quantidade${index + 1};`;
@@ -87,6 +97,26 @@ export default function DateComponent({searchPedidos, getData}) {
         <div className='flex flex-row w-full justify-center gap-10'>
             <div>
             <RangePicker format={'DD/MM/YYYY'} locale={locale} defaultValue={dateRange} onChange={handleDateChange} />
+            </div>
+                        <div>
+                        <Select
+                        defaultValue={"selecione..."}
+                
+                style={{
+                    width: 120,
+                }}
+                onChange={handleSelectChange}
+                options={[
+                    {
+                    value: '9',
+                    label: 'RotaInterior',
+                    },
+                    {
+                    value: '8',
+                    label: 'RotaCidade',
+                    },
+                ]}
+                />
             </div>
             {data.length > 0 && (
             <div>
