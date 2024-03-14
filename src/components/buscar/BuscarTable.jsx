f9383c31af7b6c3d6870e4ab6ce37c14cd8bde0d
crokids <148979461+crokids@users.noreply.github.com>
2024-03-14 19:51:01 -0300

    Update BuscarTable.jsx

--- src/components/buscar/BuscarTable.jsx
+++ src/components/buscar/BuscarTable.jsx
@@ -1,62 +1,99 @@
 'use client'
 
 import React from 'react';
-import { Space, Table } from 'antd';
+import { Space, Table, Modal, message } from 'antd';
+import { ExclamationCircleFilled } from '@ant-design/icons';
 
-const columns = [
-  {
-    title: 'Data',
-    dataIndex: 'data',
-    key: 'data',
-  },
-  {
-    title: 'Cod_cli',
-    dataIndex: 'cliente',
-    key: 'cliente',
-  },
-  {
-    title: 'Cod_rota',
-    dataIndex: 'cod_rota',
-    key: 'cod_rota',
-  },
-  {
-    title: 'NF',
-    dataIndex: 'nf',
-    key: 'nf',
-  },
-  {
-    title: 'Tipo_pgt',
-    dataIndex: 'tipo_pgt',
-    key: 'tipo_pgt',
-  },
-  {
-    title: 'Produtos',
-    dataIndex: 'produtos',
-    key: 'produtos',
-  },
-  {
-    title: 'OBS',
-    dataIndex: 'obs',
-    key: 'obs',
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Editar</a>
-        <a>Deletar</a>
-      </Space>
-    ),
-  },
-];
+const { confirm } = Modal;
 
-export default function BuscarTable({tableData}) {
+
+
+export default function BuscarTable({tableData, deletePedido, removeObjectByKey}) {
+
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const success = () => {
+    messageApi.open({
+      type: 'success',
+      content: 'Pedido deletado com sucesso!',
+    });
+  };
+
+  const showDeleteConfirm = (id) => {
+    confirm({
+      title: 'Você tem certeza que deseja deletar esse pedido?',
+      icon: <ExclamationCircleFilled />,
+      content: 'para deletar clique em "SIM"',
+      okText: 'Sim',
+      okType: 'danger',
+      cancelText: 'Não',
+      async onOk() {
+       const res = await deletePedido(id);
+       removeObjectByKey(id)
+         if(res === "OK") {
+            success()
+       }
+      },
+      onCancel() {
+      },
+    });
+  };
+
+  const columns = [
+    {
+      title: 'Data',
+      dataIndex: 'data',
+      key: 'data',
+    },
+    {
+      title: 'Cod_cli',
+      dataIndex: 'cliente',
+      key: 'cliente',
+    },
+    {
+      title: 'Cod_rota',
+      dataIndex: 'cod_rota',
+      key: 'cod_rota',
+    },
+    {
+      title: 'NF',
+      dataIndex: 'nf',
+      key: 'nf',
+    },
+    {
+      title: 'Tipo_pgt',
+      dataIndex: 'tipo_pgt',
+      key: 'tipo_pgt',
+    },
+    {
+      title: 'Produtos',
+      dataIndex: 'produtos',
+      key: 'produtos',
+    },
+    {
+      title: 'OBS',
+      dataIndex: 'obs',
+      key: 'obs',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <a onClick={() => handleDelete(record.key)}>Deletar</a>
+        </Space>
+      ),
+    },
+  ];
 
   if(!tableData) {
 
     return 
     
+  }
+
+  const handleDelete = (id) => {
+      showDeleteConfirm(id)
   }
 
 
@@ -92,6 +129,7 @@ export default function BuscarTable({tableData}) {
   return (
     <div className='flex w-full h-full justify-center'>
         <Table style={{width: '80%'}} columns={columns} dataSource={transformedData} />
+        {contextHolder}
     </div>
   )
 }
